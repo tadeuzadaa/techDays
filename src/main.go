fce5671656f72f68f928d5f4151f7aab1c6a2b47
João <115498450+tadeuzadaa@users.noreply.github.com>
2025-05-29 00:49:36 +0000

    form ok

--- src/main.go
+++ src/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello Word")
+func main() {
+    var nome string
+    var idade int
+
+    fmt.Print("Digite seu nome: ")
+    fmt.Scanln(&nome)
+
+    fmt.Print("Digite sua idade: ")
+    fmt.Scanln(&idade)
+
+    fmt.Printf("Nome: %s, Idade: %d\n", nome, idade)
 }
+
